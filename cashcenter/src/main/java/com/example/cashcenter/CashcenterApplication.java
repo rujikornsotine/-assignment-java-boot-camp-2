d23a9f87c42788017c43b4fa126543a7887b0861
rujikorn.so.tine <rujikorn.so.tine@gmail.com>
2022-03-16 15:42:28 +0700

    feat: ตั้งโครงสร้าง CashCenter

---
+++ cashcenter/src/main/java/com/example/cashcenter/CashcenterApplication.java
@@ -0,0 +1,13 @@
+package com.example.cashcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashcenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashcenterApplication.class, args);
+	}
+
+}
---
+++ cashcenter/src/test/java/com/example/cashcenter/CashcenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cashcenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashcenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
